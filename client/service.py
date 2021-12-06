0ef2e1ab1f381558cb0965fac9e283a464e8579d
thCorOli <93624142+thCorOli@users.noreply.github.com>
2021-12-06 19:44:24 -0300

    Prototipo de funcao com socket

--- client/service.py
+++ client/service.py
@@ -5,12 +5,12 @@ localPort = "44110"
 
 # Create a datagram socket
 socketServerFront = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-socketServerFront.bind((localIP, localPort))
 
 def listVideos ():
     socketServerFront.connect_ex(localIP,localPort)
     videos = socketServerFront.send( None,'LISTAR_VIDEO')
     print(videos)
+    socketServerFront.close()
 
 def playVideos():
     print('play')
