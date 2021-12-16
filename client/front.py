917381952aaaffad1d7d9eefcacafcec86f07785
thCorOli <93624142+thCorOli@users.noreply.github.com>
2021-12-16 16:06:59 -0300

    Atualizando a renderezição do listVideos

--- client/front.py
+++ client/front.py
@@ -15,28 +15,24 @@ red = '#E50914'
 window.configure(background=backgroundColor)
 style.configure("TButton", font =
                ('calibri',10,'bold'),
-                foreground = red,borderwidth=4,anchor="center",_Padding='10')
+                foreground = red,borderwidth=4,anchor="center")
 style.configure('LabelList',fontColor=red)
 
 def reciveListVideos():
-    posX = 200
+    posX = 230
     posY = 200
-    listVideos = service.listVideos()
-    for video in listVideos:
-        videoButton = ttk.Button(textvariable=video, master=window, command = playVideo(video) ,style="TButton")
-        videoButton.place(posX,posY,width=100,height=100)
-        posX+=20
-        posY+=20
+    #listOfVideos = service.listVideos()
+    listOfVideos = ['Teste1','Teste2']
+    for video in listOfVideos:
+        videoButton = ttk.Button(text=video, master=window)
+        videoButton["command"] = lambda videoButton=videoButton: playVideo(video)
+        videoButton.place(x=posX,y=posY,width=50,height=25)
+        posY+=40
 
 def playVideo(videoTitle):
-    playedVideo = service.playVideos(videoTitle)
+    print('Titulo quando eu clico',videoTitle)
+    #playedVideo = service.playVideos(videoTitle)
     #renderizar o broadCast?
-
-
-def playVideoButton():
-    playButton = ttk.Button(text="Streamar", master=window, command = playVideo ,style="TButton")
-    playButton.place(width=80,height=20)
-    playButton.pack(pady=4)
 
 def listVideoButton():
     listButton = ttk.Button(text="Listar", master=window, command = reciveListVideos,style="TButton")
@@ -48,7 +44,6 @@ def start():
     window.title("Redes 2")
     window.geometry("500x300")
     listVideoButton()
-    playVideoButton()
     window.mainloop()
 
 start()
